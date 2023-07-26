824ad4e08dd9519ada612d6f42eb91a3df42788e
Nitin1604 <nitinsokhal@gmail.com>
2023-07-26 22:50:00 +0530

    Flat and Flat Map

--- Flat and Flat Map.js
+++ Flat and Flat Map.js
@@ -1,7 +1,7 @@
 console.log('Flat and Flat Map in js')
 
-const flatArr = [[1, 2, 3], [4, 5, 6], 7, 8]
-console.log('flatArr:',flatArr.flat()) 
-
-const flatInnerArr = [[[1, 2],3], [4, [5, 6]], 7, 8]
-console.log('flarInnerArr:',flatInnerArr.flat())  
+const flatArr = [[1, 2, 3], [4, 5, 6], 7, 8] 
+console.log('flatArr:',flatArr.flat())  
+ 
+const flatInnerArr = [[[1, 2],3], [4, [5, 6]], 7, 8] 
+console.log('flarInnerArr:',flatInnerArr.flat())   
