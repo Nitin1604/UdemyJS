f44446c8593c38aafea154a22235ade588c68703
Nitin1604 <nitinsokhal@gmail.com>
2023-07-26 23:03:41 +0530

    udemyJS commit

--- Flat and Flat Map.js
+++ Flat and Flat Map.js
@@ -4,4 +4,4 @@ const flatArr = [[1, 2, 3], [4, 5, 6], 7, 8]
 console.log('flatArr:',flatArr.flat())  
  
 const flatInnerArr = [[[1, 2],3], [4, [5, 6]], 7, 8] 
-console.log('flarInnerArr:',flatInnerArr.flat())    
+console.log('flarInnerArr:',flatInnerArr.flat())     
