b8a3cb0413ef0d581c919fa0e5903e5b8e8d244c
Nitin1604 <nitinsokhal@gmail.com>
2023-07-26 23:28:47 +0530

    updated code

--- Flat and Flat Map.js
+++ Flat and Flat Map.js
@@ -5,3 +5,33 @@ console.log('flatArr:',flatArr.flat())
  
 const flatInnerArr = [[[1, 2],3], [4, [5, 6]], 7, 8] 
 console.log('flarInnerArr:',flatInnerArr.flat())     
+
+const account1 = {
+    owner: 'Himanshi',
+    movements: [164, 156, -123, 2000, -189, -130, 70, 1300],
+    interestRate: 1.2, 
+    pin: 111,
+  };
+  
+  const account2 = {
+    owner: 'Himanshu',
+    movements: [5000, 3123, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 222,
+  };
+  
+  const account3 = {
+    owner: 'Deepanshu',
+    movements: [164, -164, 340, -300, -20, 50, 123, -460],
+    interestRate: 0.7,
+    pin: 333,
+  };
+  
+  const account4 = {
+    owner: 'Deepanshi',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 444,
+  };
+
+  const accounts = [account1, account2, account3, account4];
